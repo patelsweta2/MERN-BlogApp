1a0eb1b1b5415df269a62eb604fc987db1f5428f
patelsweta2 <sweta0442@gmail.com>
2024-01-25 19:56:32 +0530

    create a complete sign-up page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,20 +3,26 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const Header = () => {
   const path = useLocation().pathname;
   return (
     <Navbar className="border-b-4">
-      <Link
-        to="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
       >
-        <span className="px-2 py-1 bg-gradient-to-r from-yellow-500 via-red-500 to-yellow-500 rounded-lg text-white">
-          NamasteNest
-        </span>
-        Blog
-      </Link>
+        <Link
+          to="/"
+          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        >
+          <span className="px-2 py-1 bg-gradient-to-r from-yellow-500 via-red-500 to-yellow-500 rounded-lg text-white">
+            NamasteNest
+          </span>
+          Blog
+        </Link>
+      </motion.div>
       <form>
         <TextInput
           type="text"
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { Button, Label, TextInput } from "flowbite-react";
 
-function Signup() {
-  return <div>Signup</div>;
+function SignUp() {
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        <div className="flex-1">
+          <motion.div
+            whileInView={{ y: [-100, 0], opacity: [0, 1] }}
+            transition={{ duration: 0.5 }}
+          >
+            <Link to="/" className="font-bold dark:text-white text-4xl">
+              <span className="px-2 py-1 bg-gradient-to-r from-yellow-500 via-red-500 to-yellow-500 rounded-lg text-white">
+                NamasteNest
+              </span>
+              Blog
+            </Link>
+          </motion.div>
+          <p className="text-sm mt-5">
+            Embark on a journey of words. Join our blogging community and let
+            your thoughts take flight
+          </p>
+        </div>
+        <div className="flex-1">
+          <motion.div
+            whileInView={{ y: [-100, 0], opacity: [0, 1] }}
+            transition={{ duration: 0.5 }}
+          >
+            <form className="flex flex-col gap-4">
+              <div>
+                <Label value="Your username" />
+                <TextInput type="text" placeholder="Username" id="username" />
+              </div>
+              <div>
+                <Label value="Your email" />
+                <TextInput
+                  type="text"
+                  placeholder="name@gmail.com"
+                  id="email"
+                />
+              </div>
+              <div>
+                <Label value="Your password" />
+                <TextInput type="text" placeholder="Password" id="password" />
+              </div>
+              <Button gradientDuoTone="pinkToOrange" type="submit">
+                Sign Up
+              </Button>
+            </form>
+            <div className="flex gap-2 text-sm mt-5">
+              <span>Have an account?</span>
+              <Link to="/sign-in" className="text-blue-500">
+                Sign In
+              </Link>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Signup;
+export default SignUp;
