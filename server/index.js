ad7643e98871393ab9cf6859fc7539eaf417ea50
patelsweta2 <sweta0442@gmail.com>
2024-01-25 15:56:48 +0530

    add middleware and function to handle a error

--- server/index.js
+++ server/index.js
@@ -24,3 +24,13 @@ app.listen(4000, () => {
 
 app.use("/server/user", userRoutes);
 app.use("/server/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
