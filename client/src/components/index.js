4af22f8561594497d199774905987807e541128b
patelsweta2 <sweta0442@gmail.com>
2024-01-27 18:29:18 +0530

    complete sidebar of the dashboard

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,5 @@ export { default as Footer } from "./Footer";
 export { default as Oauth } from "./Oauth";
 export { default as ThemeProvider } from "./ThemeProvider";
 export { default as PrivateRoute } from "./PrivateRoute";
+export { default as DashSidebar } from "./DashSideBar";
+export { default as DashProfile } from "./DashProfile";
