674a6f428f4500c6d8d34cc6ee9a7785f559860d
patelsweta2 <sweta0442@gmail.com>
2024-01-30 14:58:45 +0530

    add delete post functionality

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -65,3 +65,15 @@ export const getposts = async (req, res, nest) => {
     next(error);
   }
 };
+
+export const deletepost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete the post"));
+  }
+  try {
+    await Post.findByIdAndDelete(req.params.postId);
+    res.status(200).json("The post has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
