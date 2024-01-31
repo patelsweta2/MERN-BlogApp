c20e68dfdeae3df2f8c65f4269cbbf80c94f5cf7
patelsweta2 <sweta0442@gmail.com>
2024-01-31 11:14:19 +0530

    add delete user functionality

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -47,7 +47,22 @@ const DashUsers = () => {
     }
   };
 
-  const handleDeleteUser = async () => {};
+  const handleDeleteUser = async () => {
+    try {
+      const res = await fetch(`/server/user/delete/${userIdToDelete}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (res.ok) {
+        setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
+        setShowModal(false);
+      } else {
+        console.log(data.message);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div
