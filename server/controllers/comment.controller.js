4b288ba9ee8d6df91e0f4192daf7d92e0c0fd99a
patelsweta2 <sweta0442@gmail.com>
2024-01-31 20:43:30 +0530

    show the comments of a post

--- server/controllers/comment.controller.js
+++ server/controllers/comment.controller.js
@@ -1,5 +1,6 @@
 import errorHandler from "../utils/error.js";
 import Comment from "../models/comment.model.js";
+
 export const createComment = async (req, res, next) => {
   try {
     const { content, postId, userId } = req.body;
@@ -15,6 +16,17 @@ export const createComment = async (req, res, next) => {
     });
     await newComment.save();
     res.status(200).json(newComment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPostComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ postId: req.params.postId }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json(comments);
   } catch (error) {
     next(error);
   }
